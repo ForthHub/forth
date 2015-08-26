e470934cffab7d62ae042ea010d1878dce26784c
Aliaksei Chapyzhenka <alex.drom@gmail.com>
2015-08-25 22:08:48 -0700

    implemented streaming quit

--- test/basic.js
+++ test/basic.js
@@ -3,6 +3,23 @@
 var forth = require('../lib'),
     expect = require('chai').expect;
 
+function run (str, ds, rs) {
+    forth.RS = [];
+    forth.DS = [];
+    str
+    .split(' ')
+    .forEach(function (name) {
+        var op;
+        op = forth[name];
+        if (op) {
+            op.call(forth);
+        } else {
+            forth.DS.push(Number(name));
+        }
+    });
+    expect([forth.DS, forth.RS]).to.deep.equal([ds, rs]);
+}
+
 describe('#basic', function () {
     it('#words', function () {
         forth.words();
@@ -10,9 +27,29 @@ describe('#basic', function () {
     it('#see', function () {
         forth.see(forth.see);
     });
-    it('#minus', function () {
-        forth.DS = [7, 3];
-        forth['-']();
-        expect(forth.DS).to.deep.equal([4]);
+    it('#stack', function () {
+        run('0 0 and', [0], []);
+        run('0 1 and', [0], []);
+        run('1 0 and', [0], []);
+        run('1 1 and', [1], []);
+
+        run('0 invert 1 and', [1], []);
+        run('1 invert 1 and', [0], []);
+
+        run('1 2*', [2], []);
+        run('4000 2/', [2000], []);
+        run('1 15 lshift', [32768], []);
+        run('4 2 rshift', [1], []);
+        run('0 0=', [true], []);
+        run('-1 0=', [false], []);
+
+        run('-5 9 +', [4], []);
+        run('7 3 -', [4], []);
+        run('drop', [], []);
+    });
+    it('#stream', function () {
+        forth.quit();
+        forth.inputStream.end('123 456 - ');
+        expect(forth.DS).to.deep.equal([-333]);
     });
 });
