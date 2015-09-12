9ea168547053f7ea811bf50752309a9101916f53
Aliaksei Chapyzhenka <alex.drom@gmail.com>
2015-09-12 11:31:20 -0700

    added interpreter

---
+++ test/interpret.js
@@ -0,0 +1,55 @@
+'use strict';
+
+var stack = require('../lib/stack'),
+    memory = require('../lib/memory'),
+    // word = require('../lib/word'),
+    // create = require('../lib/create'),
+    interpret = require('../lib/interpret'),
+
+    expect = require('chai').expect;
+
+describe('#interpret', function () {
+
+    it('#stack', function (done) {
+        var test = {
+            L: [],
+            O: {},
+            interpret: interpret
+        };
+        stack(test);
+
+        expect(test.DS).to.deep.equal([]);
+
+        test.interpret(' 6    7     ');
+        expect(test.DS).to.deep.equal([6, 7]);
+
+        test.interpret(' swap  ');
+        expect(test.DS).to.deep.equal([7, 6]);
+
+        test.interpret(' dup 5 nip ');
+        expect(test.DS).to.deep.equal([7, 6, 5]);
+
+        test.interpret(' drop over * nip');
+        expect(test.DS).to.deep.equal([42]);
+
+        done();
+    });
+
+    it('#memory', function (done) {
+        var test = {
+            L: [],
+            O: {},
+            interpret: interpret
+        };
+        stack(test);
+        memory(test);
+
+        expect(test.DS).to.deep.equal([]);
+
+        test.interpret(' 6 0 !   7 4 !  0 @ 4 @ *  ');
+
+        expect(test.DS).to.deep.equal([42]);
+
+        done();
+    });
+});
