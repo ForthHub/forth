e66486e78a15b010416352c94839cfd3ab0ec6e5
Aliaksei Chapyzhenka <alex.drom@gmail.com>
2015-11-07 23:09:51 -0800

    make word and parse go past delimeter

--- test/word.js
+++ test/word.js
@@ -17,7 +17,6 @@ function string2ArrayBuffer (str) {
 
 describe('#word', function () {
     it('#string 0', function (done) {
-        var delimeter = '\\s';
         var buf = 'ABC  abc de \nf\n\nXYZ';
         var test = {
             buf: buf,
@@ -25,17 +24,16 @@ describe('#word', function () {
             last: 15,
             word: word.str
         };
-        expect(test.word(delimeter)).to.be.equal('abc');
-        expect(test.word(delimeter)).to.be.equal('de');
-        expect(test.word(delimeter)).to.be.equal('f');
-        expect(test.word(delimeter)).to.be.equal(undefined);
-        expect(test.word(delimeter)).to.be.equal(undefined);
-        expect(test.word(delimeter)).to.be.equal(undefined);
+        expect(test.word('\\s')).to.be.equal('abc');
+        expect(test.word('\\s')).to.be.equal('de');
+        expect(test.word('\\s')).to.be.equal('f');
+        expect(test.word('\\s')).to.be.equal(undefined);
+        expect(test.word('\\s')).to.be.equal(undefined);
+        expect(test.word('\\s')).to.be.equal(undefined);
         expect(test.ptr).to.be.equal(16);
         done();
     });
     it('#string 1', function (done) {
-        var delimeter = '\\s';
         var buf = 'xy  \n  z';
         var test = {
             buf: buf,
@@ -43,16 +41,15 @@ describe('#word', function () {
             last: 7,
             word: word.str
         };
-        expect(test.word(delimeter)).to.be.equal('xy');
-        expect(test.word(delimeter)).to.be.equal('z');
-        expect(test.word(delimeter)).to.be.equal(undefined);
-        expect(test.word(delimeter)).to.be.equal(undefined);
-        expect(test.word(delimeter)).to.be.equal(undefined);
+        expect(test.word('\\s')).to.be.equal('xy');
+        expect(test.word('\\s')).to.be.equal('z');
+        expect(test.word('\\s')).to.be.equal(undefined);
+        expect(test.word('\\s')).to.be.equal(undefined);
+        expect(test.word('\\s')).to.be.equal(undefined);
         expect(test.ptr).to.be.equal(8);
         done();
     });
     it('#buffer 0', function (done) {
-        var delimeter = '\\s';
         var buf = new Buffer('ABC  abc de \nf\n\nXYZ', 'ascii');
         var test = {
             buf: buf,
@@ -60,17 +57,16 @@ describe('#word', function () {
             last: 15,
             word: word.buf
         };
-        expect(test.word(delimeter)).to.be.equal('abc');
-        expect(test.word(delimeter)).to.be.equal('de');
-        expect(test.word(delimeter)).to.be.equal('f');
-        expect(test.word(delimeter)).to.be.equal(undefined);
-        expect(test.word(delimeter)).to.be.equal(undefined);
-        expect(test.word(delimeter)).to.be.equal(undefined);
+        expect(test.word('\\s')).to.be.equal('abc');
+        expect(test.word('\\s')).to.be.equal('de');
+        expect(test.word('\\s')).to.be.equal('f');
+        expect(test.word('\\s')).to.be.equal(undefined);
+        expect(test.word('\\s')).to.be.equal(undefined);
+        expect(test.word('\\s')).to.be.equal(undefined);
         expect(test.ptr).to.be.equal(16);
         done();
     });
     it('#buffer 1', function (done) {
-        var delimeter = '\\s';
         var buf = new Buffer('xy  \n  z', 'ascii');
         var test = {
             buf: buf,
@@ -78,16 +74,15 @@ describe('#word', function () {
             last: 7,
             word: word.buf
         };
-        expect(test.word(delimeter)).to.be.equal('xy');
-        expect(test.word(delimeter)).to.be.equal('z');
-        expect(test.word(delimeter)).to.be.equal(undefined);
-        expect(test.word(delimeter)).to.be.equal(undefined);
-        expect(test.word(delimeter)).to.be.equal(undefined);
+        expect(test.word('\\s')).to.be.equal('xy');
+        expect(test.word('\\s')).to.be.equal('z');
+        expect(test.word('\\s')).to.be.equal(undefined);
+        expect(test.word('\\s')).to.be.equal(undefined);
+        expect(test.word('\\s')).to.be.equal(undefined);
         expect(test.ptr).to.be.equal(8);
         done();
     });
     it('#array 0', function (done) {
-        var delimeter = '\\s';
         var buf = string2ArrayBuffer('ABC  abc de \nf\n\nXYZ');
         var test = {
             buf: buf,
@@ -95,17 +90,16 @@ describe('#word', function () {
             last: 15,
             word: word.arr
         };
-        expect(test.word(delimeter)).to.be.equal('abc');
-        expect(test.word(delimeter)).to.be.equal('de');
-        expect(test.word(delimeter)).to.be.equal('f');
-        expect(test.word(delimeter)).to.be.equal(undefined);
-        expect(test.word(delimeter)).to.be.equal(undefined);
-        expect(test.word(delimeter)).to.be.equal(undefined);
+        expect(test.word('\\s')).to.be.equal('abc');
+        expect(test.word('\\s')).to.be.equal('de');
+        expect(test.word('\\s')).to.be.equal('f');
+        expect(test.word('\\s')).to.be.equal(undefined);
+        expect(test.word('\\s')).to.be.equal(undefined);
+        expect(test.word('\\s')).to.be.equal(undefined);
         expect(test.ptr).to.be.equal(16);
         done();
     });
     it('#array 1', function (done) {
-        var delimeter = '\\s';
         var buf = string2ArrayBuffer('xy  \n  z');
         var test = {
             buf: buf,
@@ -113,12 +107,32 @@ describe('#word', function () {
             last: 7,
             word: word.arr
         };
-        expect(test.word(delimeter)).to.be.equal('xy');
-        expect(test.word(delimeter)).to.be.equal('z');
-        expect(test.word(delimeter)).to.be.equal(undefined);
-        expect(test.word(delimeter)).to.be.equal(undefined);
-        expect(test.word(delimeter)).to.be.equal(undefined);
+        expect(test.word('\\s')).to.be.equal('xy');
+        expect(test.word('\\s')).to.be.equal('z');
+        expect(test.word('\\s')).to.be.equal(undefined);
+        expect(test.word('\\s')).to.be.equal(undefined);
+        expect(test.word('\\s')).to.be.equal(undefined);
         expect(test.ptr).to.be.equal(8);
+        done();
+    });
+});
+
+describe('#take', function () {
+    it('#string 0', function (done) {
+        var buf = 'ABC  abc de \nf\n\nXYZ';
+        var test = {
+            buf: buf,
+            ptr: 0,
+            last: 15,
+            take: word.take
+        };
+        expect(test.take('\\s')).to.be.equal('ABC');
+        expect(test.take('c')).to.be.equal(' ab');
+        expect(test.take('f')).to.be.equal(' de \n');
+        expect(test.take('Y')).to.be.equal('\n\n');
+        expect(test.take('\\s')).to.be.equal('');
+        expect(test.take('\\s')).to.be.equal('');
+        expect(test.ptr).to.be.equal(16);
         done();
     });
 });
