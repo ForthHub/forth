0d57aaf4edbe6c45f881e9157f11985e2e02db48
Aliaksei Chapyzhenka <alex.drom@gmail.com>
2015-11-13 16:14:27 -0800

    fixed #1 ; added REPL

--- bin/forth.js
+++ bin/forth.js
@@ -3,25 +3,31 @@
 
 var
     concat = require('concat-stream'),
-    // colors = require('colors/safe'),
-    // repl = require('repl'),
+    colors = require('colors/safe'),
+    repl = require('repl'),
     forth = require('../lib');
 
 // create clean forth object
 var f = forth();
 
-// console.log('forth v%s', f.pkg.version);
-//
-// repl.start({
-//     prompt: colors.green('\u25B6 '),
-//     input: process.stdin,
-//     output: process.stdout,
-//     eval: function (cmd, context, filename, callback) {
-//         var result = cmd.trim();
-//         forth[result]();
-//         callback(null, result);
-//     }
-// });
+console.log('forth v%s', f.pkg.version);
+
+var buffer = '';
+
+process.stdin.on('data', function(chunk) {
+  buffer += chunk.toString('utf8');
+});
+
+repl.start({
+    prompt: colors.green('\u25B6 '),
+    input: process.stdin,
+    output: process.stdout,
+    eval: function (cmd, context, filename, callback) {
+        var tmp = buffer;
+        buffer = '';
+        f.interpret(tmp, callback);
+    }
+});
 
 
 
